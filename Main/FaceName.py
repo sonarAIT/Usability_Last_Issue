09f9863226e06d768c676fabccaf4d76b928c903
sonarAIT <sonarmikumiku@gmail.com>
2022-01-18 15:55:54 +0900

    feat: UI完成

---
+++ Main/FaceName.py
@@ -0,0 +1,5 @@
+class FaceName:
+    Funny = "./Main/img/funny.png"
+    Normal = "./Main/img/normal.png"
+    Sad = "./Main/img/sad.png"
+    Subtle = "./Main/img/subtle.png"
--- Main/UI.py
+++ Main/UI.py
@@ -1,15 +1,43 @@
+import PySimpleGUI as sg
+from FaceName import FaceName
+
+
 class UI:
-    def __init__():
-        pass
+    def __init__(self) -> None:
+        LAYOUT = [
+            [sg.Text("", font=("", 50), key="-HEAD_LABEL-")],
+            [sg.Image(FaceName.Normal, key="-FACE_IMAGE-"), sg.Text("", font=("", 30), key="-MAIN_LABEL-")],
+            [sg.Button("スタート", key="-START_BUTTON-")]
+        ]
+        self.WINDOW = sg.Window("ほげ", LAYOUT, element_justification='c')
 
-    def GetEvent():
-        pass
+    def Read(self) -> None:
+        event, values = self.WINDOW.read()
+        return (event, values)
 
-    def ChangeMainLabel():
-        pass
+    def UpdateMainLabel(self, text):
+        self.WINDOW["-MAIN_LABEL-"].Update(text)
 
-    def ChangeHeadLabel():
-        pass
+    def UpdateHeadLabel(self, text):
+        self.WINDOW["-HEAD_LABEL-"].Update(text)
 
-    def ChangeFace():
-        pass
+    def UpdateFace(self, faceName):
+        self.WINDOW["-FACE_IMAGE-"].Update(faceName)
+
+    def Close(self):
+        self.WINDOW.close()
+
+
+if __name__ == "__main__":
+    ui = UI()
+
+    while True:
+        event, values = ui.Read()
+        if event == sg.WIN_CLOSED:
+            ui.Close()
+            break
+
+        if event == "-START_BUTTON-":
+            ui.UpdateMainLabel("ウホウホウホウホウホウホウホウホウホウホ")
+            ui.UpdateHeadLabel("ウホウホウホウホウホ")
+            ui.UpdateFace(FaceName.Funny)

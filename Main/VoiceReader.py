28c242228e3da63dc1c8848e4b78f7df7aa6e6ec
sonarAIT <sonarmikumiku@gmail.com>
2022-01-23 04:35:47 +0900

    feat: VoiceRecorderを実装

--- Main/VoiceReader.py
+++ Main/VoiceReader.py
@@ -1,6 +1,25 @@
-class VoiceReader:
-    def __init__():
-        pass
+import speech_recognition as sr
 
-    def ReadVoice():
-        pass
+
+class VoiceReader:
+    rec = sr.Recognizer()
+    mic = sr.Microphone()
+
+    def ReadVoice(self):
+        with self.mic as source:
+            print("ノイズ収集中…")
+            self.rec.adjust_for_ambient_noise(source)
+            print("ノイズ収集完了")
+        with self.mic as source:
+            print("どうぞ")
+            audio = self.rec.listen(source)
+            try:
+                text = self.rec.recognize_google(audio, language="ja-JP")
+                return text
+            except sr.UnknownValueError:
+                return "error"
+
+
+if __name__ == "__main__":
+    voiceReader = VoiceReader()
+    print(voiceReader.ReadVoice())

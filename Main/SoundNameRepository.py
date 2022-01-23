93c06407eb961fe58c18fee01c0c092b7442cfb0
sonarAIT <sonarmikumiku@gmail.com>
2022-01-23 22:52:07 +0900

    feat: Stop実装

--- Main/SoundNameRepository.py
+++ Main/SoundNameRepository.py
@@ -1,9 +1,9 @@
 class SoundNameRepository:
     __PATH = "./Main/wav/"
-    __SOUND_NAME_DICT = {'PATH': __PATH, 'CALCING': __PATH + "calcing.wav"}
+    __SOUND_NAME_DICT = {'CALCING': __PATH + "calcing.wav"}
 
-    def get(name: str) -> str:
+    def Get(name: str) -> str:
         return SoundNameRepository.__SOUND_NAME_DICT[name]
 
-    def getAll() -> list[str]:
+    def GetAll() -> list[str]:
         return [(sound, SoundNameRepository.__SOUND_NAME_DICT[sound]) for sound in SoundNameRepository.__SOUND_NAME_DICT]
--- Main/SoundPlayer.py
+++ Main/SoundPlayer.py
@@ -1,6 +1,37 @@
+import wave
+import numpy as np
+import sounddevice as sd
+from SoundNameRepository import SoundNameRepository
+
+
 class SoundPlayer:
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.__soundMap = {}
+        for name, path in SoundNameRepository.GetAll():
+            try:
+                with wave.open(path) as wf:
+                    fs = wf.getframerate()
+                    data = wf.readframes(wf.getnframes())
+                    data = np.frombuffer(data, dtype='int16')
+
+                    self.__soundMap[name] = (data, fs)
+            except Exception as e:
+                print('ERROR: 音声読み込み中にエラーが発生しました')
+                print(type(e))
+                print(e)
 
     def PlaySound(self, soundName: str) -> None:
-        pass
+        sound = self.__soundMap[soundName]
+        sd.play(sound[0], sound[1])
+        # sd.play(sound[0], sound[1] * 2) # stereo
+
+    def StopSound(self) -> None:
+        sd.stop()
+
+
+if __name__ == "__main__":
+    import time
+    soundPlayer = SoundPlayer()
+    soundPlayer.PlaySound('CALCING')
+    time.sleep(3)
+    soundPlayer.StopSound()

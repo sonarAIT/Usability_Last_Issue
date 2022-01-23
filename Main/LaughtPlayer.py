e29cbbf53c80f389787bce62c3663beeb59a0d1a
sonarAIT <sonarmikumiku@gmail.com>
2022-01-23 18:49:52 +0900

    feat: SoundPlayer 定義

---
+++ Main/LaughtPlayer.py
@@ -0,0 +1,8 @@
+from SoundPlayer import SoundPlayer
+
+class LaughtPlayer:
+    def __init__(self, soundPlayer: SoundPlayer):
+        pass
+    
+    def PlayLaught(self):
+        pass
---
+++ Main/SoundPlayer.py
@@ -0,0 +1,6 @@
+class SoundPlayer:
+    def __init__(self):
+        pass
+
+    def PlaySound(self, soundName: str) -> None:
+        pass

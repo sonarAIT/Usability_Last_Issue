9ba6b6cda55bb232ed59c8e8c33520c998f69cbb
sonarAIT <sonarmikumiku@gmail.com>
2022-01-24 04:36:58 +0900

    feat: VoiceReader 構造変更

--- Main/VoiceReader.py
+++ Main/VoiceReader.py
@@ -2,22 +2,25 @@ import speech_recognition as sr
 
 
 class VoiceReader:
-    rec = sr.Recognizer()
-    mic = sr.Microphone()
+    ERROR_CODE = "__error__"
 
+    def __init__(self):
+        self.__rec = sr.Recognizer()
+        self.__mic = sr.Microphone()
+    
     def ReadVoice(self):
-        with self.mic as source:
+        with self.__mic as source:
             print("ノイズ収集中…")
-            self.rec.adjust_for_ambient_noise(source)
+            self.__rec.adjust_for_ambient_noise(source)
             print("ノイズ収集完了")
-        with self.mic as source:
+        with self.__mic as source:
             print("どうぞ")
-            audio = self.rec.listen(source)
+            audio = self.__rec.listen(source)
             try:
-                text = self.rec.recognize_google(audio, language="ja-JP")
+                text = self.__rec.recognize_google(audio, language="ja-JP")
                 return text
             except sr.UnknownValueError:
-                return "error"
+                return VoiceReader.ERROR_CODE
 
 
 if __name__ == "__main__":
